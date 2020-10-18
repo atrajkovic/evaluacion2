5f39af1ebf4058b6b878d33099b19e283d73caee
Araceli Gallardo <69484046+atrajkovic@users.noreply.github.com>
2020-10-19 01:54:25 +0200

    Add files via upload

---
+++ js/recetario.js
@@ -0,0 +1,5 @@
+var ingrediente1 = prompt("Ingrese el primer ingrediente que le gustaria agregar");
+var ingrediente2 = prompt("Ingrese el segundo ingrediente");
+var ingrediente3 = prompt("Ingrese el tercer ingrediente");
+
+document.write(`<ul> La receta saludable Lemonies tiene los siguientes ingredientes personalizados: <li>${ingrediente1}</li> <li>${ingrediente2}</li> <li>${ingrediente3}</li>`)
